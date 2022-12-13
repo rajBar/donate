b474dcf39d08fddd3e6357d4162c4c2ffac973a9
Gulraj Bariah <raj.uni@outlook.com>
2022-12-13 10:28:22 +0000

    removing ip from this page to increase performance

--- scripts.js
+++ scripts.js
@@ -1,18 +1,5 @@
-async function getExternalIp() {
-	const url = "https://api.ipify.org?format=json";
-	let ipAddress = "";
-	try {
-		await fetch(url)
-			.then(res => res.json())
-			.then(res => ipAddress = res.ip);
-	} catch (err) {
-		console.log(`Error: ${err}`)
-	}
-	return ipAddress;
-}
-
 async function sendTracker() {
-	const ip = await getExternalIp();
+	const ip = "None";
 	const platform = `${navigator.platform}, ${navigator.vendor}`;
 	const site = "donate";
 	const url = `https://maker.ifttt.com/trigger/site_visited/with/key/b_Yu8_AU_JIDYDYR_WXF5-?value1=${ip}&value2=${platform}&value3=${site}`;
